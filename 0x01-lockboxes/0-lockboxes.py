4f7c21f0c853f8e884bc3dc9aa5de074fed3f064
Romha Shwangzaw <romhas@gmail.com>
2023-04-25 17:13:49 +0300

    added function to solve Lockboxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Lockboxes module """
+
+
+def canUnlockAll(boxes):
+    """a method that determines if all the boxes can be opened.
+
+    Params: @boxes: a list of lists of integers.
+    Description: A key with the same number as a box opens that box.
+                 There can be keys that do not have boxes.
+                 The first box boxes[0] is unlocked.
+    Return True if all boxes can be opened, else return False
+    """
+    boxesLockStatus = [True] * len(boxes)
+    boxesLockStatus[0] = False
+    canUnlockAllRecursive(boxes, 0, boxesLockStatus)
+    for boxLockStatus in boxesLockStatus:
+        if boxLockStatus is True:
+            return False
+    return True
+
+def canUnlockAllRecursive(boxes, boxIndex, boxesLockStatus):
+    """a method that determines if all the boxes can be opened recursively.
+
+    Params: @boxes: a list of boxes.
+            @boxIndex: the index of the open box.
+            @boxesLockStatus: a list containing the lock state of the boxes.
+    Description: A key with the same number as a box opens that box.
+                 There can be keys that do not have boxes.
+                 The first box boxes[0] is unlocked.
+                 Updates the boxesLockStatus array.
+    """
+    for n in boxes[boxIndex]:
+        if n < len(boxesLockStatus) and boxesLockStatus[n] is True:
+            boxesLockStatus[n] = False
+            canUnlockAllRecursive(boxes, n, boxesLockStatus)
